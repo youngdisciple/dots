7b1b15ab2a7b0900dd428f1afa9cf15185204306
John <youngdisciple@tutamail.com>
2026-08-11 00:20:05 -0400

    fix(nvim): project plugin swapped

--- .config/nvim/lua/plugins/project.lua
+++ .config/nvim/lua/plugins/project.lua
@@ -1,7 +1,4 @@
 return {
-    'ahmedkhalf/project.nvim',
-    config = function()
-        require('project_nvim').setup {}
-        require('telescope').load_extension('projects')
-    end,
+    'DrKJeff16/project.nvim',
+    opts = {}
 }
